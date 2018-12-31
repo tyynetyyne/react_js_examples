34760a0b46199f0cfa50b278e4a8fa0ee8d54c17
Tiina Partanen <tyynetyyne@gmail.com>
2018-12-31 17:38:28 +0200

    First demo ready
    ToggleButton with React.Component and setState

--- src/App.js
+++ src/App.js
@@ -3,17 +3,39 @@ import "./styles.css";
 import NameAndAddress from "./NameAndAddress";
 import ToggleButton from "./ToggleButton";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Ensimmäinen harjoitus</h1>
-      <NameAndAddress
-        name="Tiina Partanen"
-        email="tiina.s.partanen@tampere.fi"
-      />
-      <ToggleButton show="true" />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Tiina Partanen",
+      email: "tiina.s.partanen@tampere.fi",
+      link: "https://www.linkedin.com/in/tiina-partanen-2078852/",
+      show: true
+    };
+  }
+  handleClick() {
+    // return () => {
+    console.log("button");
+    this.setState({ show: this.state.show ? false : true });
+    // };
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Ensimmäinen harjoitus</h1>
+        <NameAndAddress
+          name={this.state.name}
+          email={this.state.email}
+          show={this.state.show}
+          link={this.state.link}
+        />
+        <ToggleButton
+          show={this.state.show}
+          clickHandler={this.handleClick.bind(this)}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
